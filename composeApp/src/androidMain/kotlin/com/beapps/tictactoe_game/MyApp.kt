20cbc9bdabe00ffe7d72f4158cc2881c316ea082
Rahul Jha <132360698+Rahul93102@users.noreply.github.com>
2025-08-17 00:37:50 +0530

    MYapp modifcation

--- composeApp/src/androidMain/kotlin/com/beapps/tictactoe_game/MyApp.kt
+++ composeApp/src/androidMain/kotlin/com/beapps/tictactoe_game/MyApp.kt
@@ -12,3 +12,21 @@ class MyApp : Application() {
         }
     }
 }
+
+
+
+@Preview
+@Composable
+fun AppAndroidPreview() {
+    TicTacToeBoard(
+        modifier = Modifier.size(300.dp),
+        gameState = GameState(board = arrayOf(
+            arrayOf('X', null, null),
+            arrayOf(null, 'O', 'O'),
+            arrayOf(null, 'X', null),
+        )),
+        xPlayerColor = Color.Green,
+        oPlayerColor = Color.Blue,
+        onTurn = {_ , _ ->},
+    )
+}
